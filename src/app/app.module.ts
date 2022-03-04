8be8b096d98ee2a990da0de882d67e0e8cdc8120
Michael Urich <80188367+mturich@users.noreply.github.com>
2022-03-04 08:06:29 +0100

    Componets Part 2: mat-card design with selectedDish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatToolbarModule } from "@angular/material/toolbar";
 import { FlexLayoutModule } from "@angular/flex-layout";
 
-import { AppComponent } from './app.component';
-import { MatListModule } from '@angular/material/list';
+import { AppComponent } from "./app.component";
 
-import 'hammerjs';
-import { MenuComponent } from './menu/menu.component';
+import "hammerjs";
+import { MenuComponent } from "./menu/menu.component";
+import { MatListModule } from "@angular/material/list";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MenuComponent
-  ],
+  declarations: [AppComponent, MenuComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
