df0bfc59ab23fb84a9a15e21204ecdcc2fa85333
Michael Urich <80188367+mturich@users.noreply.github.com>
2022-03-10 21:08:38 +0100

    added 3 componets: about, home and contact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,9 @@ import { DishdetailComponent } from "./dishdetail/dishdetail.component";
 import { DishService } from "./services/dish.service";
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
@@ -25,6 +28,9 @@ import { FooterComponent } from "./footer/footer.component";
     DishdetailComponent,
     HeaderComponent,
     FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
