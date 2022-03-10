11647176bbeca8696b3a79ae91d02e6c15a84248
Michael Urich <80188367+mturich@users.noreply.github.com>
2022-03-10 21:57:11 +0100

    routing basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,27 @@
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { FlexLayoutModule } from "@angular/flex-layout";
-
-import { AppComponent } from "./app.component";
-
-import "hammerjs";
-import { MenuComponent } from "./menu/menu.component";
 import { MatListModule } from "@angular/material/list";
 import { MatGridListModule } from "@angular/material/grid-list";
 import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
+
+import "hammerjs";
+import { AppComponent } from "./app.component";
+import { MenuComponent } from "./menu/menu.component";
 import { DishdetailComponent } from "./dishdetail/dishdetail.component";
-import { DishService } from "./services/dish.service";
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+import { DishService } from "./services/dish.service";
+
 
 @NgModule({
   declarations: [
@@ -31,6 +34,7 @@ import { ContactComponent } from './contact/contact.component';
     AboutComponent,
     HomeComponent,
     ContactComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
@@ -41,6 +45,7 @@ import { ContactComponent } from './contact/contact.component';
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
+    AppRoutingModule,
   ],
   // services are provider
   providers: [DishService],
