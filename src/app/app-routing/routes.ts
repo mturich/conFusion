fcd47e34b61e1bac04826f098b0655b7c6da712a
Michael Urich <80188367+mturich@users.noreply.github.com>
2022-03-15 07:57:16 +0100

    single page application part 2

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -1,6 +1,7 @@
+import { Dish } from "./../shared/dish";
 import { PageNotFoundComponent } from "../components/page-not-found/page-not-found.component";
 import { Routes } from "@angular/router";
-
+import { DishdetailComponent } from "../components/dishdetail/dishdetail.component";
 import { MenuComponent } from "../components/menu/menu.component";
 import { HomeComponent } from "../components/home/home.component";
 import { AboutComponent } from "../components/about/about.component";
@@ -11,6 +12,7 @@ export const routes: Routes = [
   { path: "menu", component: MenuComponent },
   { path: "about", component: AboutComponent },
   { path: "contactus", component: ContactComponent },
+  { path: "dishdetail/:id", component: DishdetailComponent },
   { path: "", redirectTo: "/home", pathMatch: "full" },
   { path: "**", component: PageNotFoundComponent },
 ];
--- src/app/components/dishdetail/dishdetail.component.ts
+++ src/app/components/dishdetail/dishdetail.component.ts
@@ -1,6 +1,9 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Params, ActivatedRoute } from "@angular/router";
+import { Location } from "@angular/common";
 // import Dish class
 import { Dish } from "../../shared/dish";
+import { DishService } from "./../../service/dish.service";
 
 @Component({
   selector: "app-dishdetail",
@@ -8,11 +11,24 @@ import { Dish } from "../../shared/dish";
   styleUrls: ["./dishdetail.component.scss"],
 })
 export class DishdetailComponent implements OnInit {
-  // when data is passed from the parent to the child, it has to be imported
+  // when data is passed from the parent to the child, it has to be imported with @Import
   // It us definded as Dish class which has to be imported for that purpose
-  @Input() dish: Dish;
+  dish: Dish;
 
-  constructor() {}
+  constructor(
+    private dishService: DishService,
+    private location: Location,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    let id = this.route.snapshot.params['id']
+      
+    this.dish = this.dishService.getDish(id);
+  }
+
+  goBack():void {
+    this.location.back();
+  }
+
 }
