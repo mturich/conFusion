a074c69469b11c462f34c82b10a8bc0b452b334f
Michael Urich <80188367+mturich@users.noreply.github.com>
2022-03-25 08:42:18 +0100

    form validation with observables

--- src/app/components/dishdetail/dishdetail.component.ts
+++ src/app/components/dishdetail/dishdetail.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Params } from "@angular/router";
 import { Location } from "@angular/common";
 // import Dish class
 import { Dish } from "../../shared/dish";
 import { DishService } from "./../../service/dish.service";
+import { switchMap } from "rxjs/operators";
 
 @Component({
   selector: "app-dishdetail",
@@ -14,6 +15,9 @@ export class DishdetailComponent implements OnInit {
   // when data is passed from the parent to the child, it has to be imported with @Import
   // It us definded as Dish class which has to be imported for that purpose
   dish: Dish;
+  dishIds: string[];
+  prev: string;
+  next: string;
 
   constructor(
     private dishService: DishService,
@@ -22,14 +26,35 @@ export class DishdetailComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    let id = this.route.snapshot.params['id']
-      
-    this.dishService.getDish(id)
-      .subscribe((dish) => this.dish = dish);
+    this.dishService
+      .getDishIds()
+      .subscribe((dishIds) => (this.dishIds = dishIds));
+    
+    // every time the link is updated (is done by clicking the left/right button and loading another ID in the route)
+    // the route is changes and this triggers the switchmap function to get fetch the Dish for the changed ID
+    // the subscription makes sure that the dish and the corresponding next and previous dishes are updated. 
+    this.route.params
+      .pipe(
+        switchMap(
+          (params: Params) => this.dishService.getDish(params["id"])))
+      .subscribe((dish) => {
+        
+        this.dish = dish;
+        this.setPrevNext(dish.id);
+      });
+
+    //this.dishService.getDish(id).subscribe((dish) => (this.dish = dish));
   }
 
-  goBack():void {
+  setPrevNext(dishId: string) {
+    const index = this.dishIds.indexOf(dishId);
+    this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
+    this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
+  }
+
+
+
+  goBack(): void {
     this.location.back();
   }
-
 }
